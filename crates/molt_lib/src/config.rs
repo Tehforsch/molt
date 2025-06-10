39c44910a00016daa79c5a7f44864cfc1fd55315
Toni Peter <mail@tonipeter.de>
2025-06-10 23:47:06 +0200

    remove debug print from default

--- crates/molt_lib/src/config.rs
+++ crates/molt_lib/src/config.rs
@@ -4,7 +4,13 @@ pub struct Config {
 }
 
 impl Default for Config {
+    #[cfg(test)]
     fn default() -> Self {
         Self { debug_print: true }
+    }
+
+    #[cfg(not(test))]
+    fn default() -> Self {
+        Self { debug_print: false }
     }
 }
