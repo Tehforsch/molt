78b24ce9338c16643bb64ddefe83c5ff14ffcd7e
Toni Peter <mail@tonipeter.de>
2025-07-14 10:50:18 +0200

    Move Span, Spanned, WithSpan and SpannedPat into mod

--- crates/molt_lib/src/lib.rs
+++ crates/molt_lib/src/lib.rs
@@ -5,18 +5,18 @@ mod ctx;
 mod match_ctx;
 mod match_pattern;
 mod node_list;
+mod span;
 
 pub use cmp_syn::CmpSyn;
 pub use config::Config;
-pub use ctx::{
-    Ctx, GetKind, Id, NodeId, Pattern, Span, Spanned, SpannedPat, ToNode, Var, VarDecl, WithSpan,
-};
+pub use ctx::{Ctx, GetKind, Id, NodeId, Pattern, ToNode, Var, VarDecl};
 pub use match_ctx::{MatchCtx, MatchPatternData};
 pub use match_pattern::{Binding, Match, Matcher, PatType, match_pattern};
 pub use node_list::{
     List, ListMatchingMode, NoPunct, NodeList, PatNodeList, RealNodeList, Set, SetMatchingMode,
     Single, SingleMatchingMode,
 };
+pub use span::{Span, Spanned, SpannedPat, WithSpan};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum ParsingMode {
---
+++ crates/molt_lib/src/span.rs
@@ -0,0 +1,179 @@
+use std::marker::PhantomData;
+
+use crate::{Id, NodeId, Pattern};
+
+#[derive(Copy, Clone, Debug)]
+pub struct Span {
+    start: usize,
+    end: usize,
+    fake: bool,
+}
+
+impl Span {
+    pub fn new(start: usize, end: usize) -> Self {
+        Self {
+            start,
+            end,
+            fake: false,
+        }
+    }
+
+    pub fn byte_range(&self) -> std::ops::Range<usize> {
+        if self.fake {
+            panic!("Fake span");
+        }
+        self.start..self.end
+    }
+
+    pub fn fake() -> Self {
+        Span {
+            start: 0,
+            end: 0,
+            fake: true,
+        }
+    }
+
+    pub fn join(&self, span: impl Into<Span>) -> Span {
+        let span = span.into();
+        if self.fake || span.fake {
+            panic!();
+        }
+        Span {
+            start: self.start.min(span.start),
+            end: self.end.max(span.end),
+            fake: false,
+        }
+    }
+}
+
+impl From<std::ops::Range<usize>> for Span {
+    fn from(value: std::ops::Range<usize>) -> Self {
+        Self {
+            start: value.start,
+            end: value.end,
+            fake: false,
+        }
+    }
+}
+
+pub trait WithSpan: Sized {
+    fn with_span(self, span: Span) -> Spanned<Self> {
+        Spanned::new(self, span)
+    }
+
+    fn pattern_with_span(self, span: Span) -> Spanned<Pattern<Self, Id>> {
+        Spanned::new(self, span).as_pattern()
+    }
+}
+
+impl<T: Sized> WithSpan for T {}
+
+#[derive(Debug)]
+pub struct Spanned<T> {
+    span: Span,
+    pub(crate) item: T,
+}
+
+impl<T> Spanned<T> {
+    fn new(item: T, span: Span) -> Self {
+        Self { span, item }
+    }
+
+    pub fn map<S>(self, f: impl Fn(T) -> S) -> Spanned<S> {
+        Spanned {
+            span: self.span,
+            item: f(self.item),
+        }
+    }
+
+    pub fn join<S>(&self, rhs: &Spanned<S>) -> Span {
+        self.span.join(rhs.span)
+    }
+
+    pub fn span(&self) -> Span {
+        self.span
+    }
+
+    pub fn take(self) -> T {
+        self.item
+    }
+
+    pub fn as_pattern(self) -> SpannedPat<T> {
+        self.map(|item| Pattern::Real(item))
+    }
+
+    pub fn decompose(self) -> (Span, T) {
+        (self.span, self.item)
+    }
+
+    fn set_span(&mut self, span: Span) {
+        self.span = span
+    }
+}
+
+pub type SpannedPat<T> = Spanned<Pattern<T, Id>>;
+
+impl<T> SpannedPat<T> {
+    pub fn get_property<S>(&self, f: impl Fn(&T) -> S, default: S) -> S {
+        self.real().map(f).unwrap_or(default)
+    }
+
+    pub fn is_var(&self) -> bool {
+        matches!(self.item, Pattern::Pat(_))
+    }
+
+    fn real(&self) -> Option<&T> {
+        self.item.real()
+    }
+
+    pub fn unwrap_real(self) -> Spanned<T> {
+        self.map(|item| item.unwrap_real())
+    }
+
+    fn unwrap_var(self) -> NodeId<T> {
+        match self.item {
+            Pattern::Pat(t) => t.typed(),
+            Pattern::Real(_) => panic!("unwrap_real called on real variant."),
+        }
+    }
+
+    fn do_if_real(&mut self, f: impl Fn(&mut T)) {
+        match &mut self.item {
+            Pattern::Real(t) => f(t),
+            Pattern::Pat(_) => {}
+        }
+    }
+
+    pub fn map_real<S>(self, f: impl Fn(T) -> S) -> SpannedPat<S> {
+        self.map(|item| match item {
+            Pattern::Real(t) => Pattern::Real(f(t)),
+            Pattern::Pat(id) => Pattern::Pat(id),
+        })
+    }
+}
+
+impl<T> Spanned<Option<T>> {
+    pub fn transpose(self) -> Option<Spanned<T>> {
+        match self.item {
+            Some(item) => Some(Spanned {
+                span: self.span,
+                item,
+            }),
+            None => None,
+        }
+    }
+}
+
+impl<T> std::ops::Deref for Spanned<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.item
+    }
+}
+
+impl<T> std::ops::DerefMut for Spanned<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.item
+    }
+}
