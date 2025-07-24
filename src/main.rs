70c2287480f3fecbbd4b0bc78d36cf81bb55eda6
Toni Peter <mail@tonipeter.de>
2025-07-24 18:05:35 +0200

    Allow specifying which files/folders to run on

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ enum Error {
     Walkdir(#[from] walkdir::Error),
     #[error("No Cargo.toml found.")]
     CargoTomlNotFound,
+    #[error("Unknown file type for input file: {0}")]
+    UnknownInputFileType(String),
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -37,9 +39,9 @@ fn get_cargo_toml(mut path: PathBuf) -> Result<PathBuf> {
     Err(Error::CargoTomlNotFound)
 }
 
-fn get_cargo_source_files(path: &Path) -> Result<Vec<PathBuf>> {
+fn get_rust_files_from_dir(dir: &Path) -> Result<Vec<PathBuf>> {
     let mut src_files = vec![];
-    for entry in WalkDir::new(path.parent().unwrap()) {
+    for entry in WalkDir::new(dir) {
         let entry = entry?;
         let path = entry.path();
         let is_rust_src = path.extension().is_some_and(|ext| ext == "rs");
@@ -50,6 +52,28 @@ fn get_cargo_source_files(path: &Path) -> Result<Vec<PathBuf>> {
     Ok(src_files)
 }
 
+fn get_rust_files_from_paths(paths: &[PathBuf]) -> Result<Vec<PathBuf>> {
+    let mut src_files = vec![];
+    for path in paths {
+        if path.is_file() {
+            if path.extension().is_some_and(|ext| ext == "rs") {
+                src_files.push(path.to_owned());
+            } else {
+                return Err(Error::UnknownInputFileType(
+                    path.to_string_lossy().to_string(),
+                ));
+            }
+        } else if path.is_dir() {
+            src_files.extend(get_rust_files_from_dir(path)?);
+        }
+    }
+    Ok(src_files)
+}
+
+fn get_cargo_source_files(path: &Path) -> Result<Vec<PathBuf>> {
+    get_rust_files_from_dir(path.parent().unwrap())
+}
+
 fn main() -> Result<()> {
     let args = CliArgs::parse();
     let from_cargo = |path| {
@@ -57,14 +81,10 @@ fn main() -> Result<()> {
         let root = cargo_toml.parent().unwrap();
         Ok::<_, Error>((Some(root.to_owned()), get_cargo_source_files(&cargo_toml)?))
     };
-    let (root, source_files) = if let Some(path) = args.input_file {
-        if path.is_dir() {
-            from_cargo(path)?
-        } else {
-            (None, vec![path.to_owned()])
-        }
-    } else {
+    let (root, source_files) = if args.input_files.is_empty() {
         from_cargo(std::env::current_dir()?)?
+    } else {
+        (None, get_rust_files_from_paths(&args.input_files)?)
     };
     let mut input = Input::new(MoltSource::file(&args.transform_file).unwrap())
         .with_rust_src_files(source_files.iter())
