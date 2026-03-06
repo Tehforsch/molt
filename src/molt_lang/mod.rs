5fa4863d03d649a6c7ab493cf4380b74147f1535
Toni Peter <mail@tonipeter.de>
2026-03-06 08:40:30 +0100

    Move resolver into its own file.

--- src/molt_lang/mod.rs
+++ src/molt_lang/mod.rs
@@ -1,9 +1,7 @@
 mod context;
 mod grammar;
 mod interpreter;
-
-use std::cell::RefCell;
-use std::rc::Rc;
+mod resolve;
 
 pub(crate) use context::Context;
 pub(crate) use grammar::FileStructureError;
@@ -14,12 +12,10 @@ use codespan_reporting::files::Files;
 
 use crate::Ctx;
 use crate::Id;
-use crate::Var;
-use crate::parser;
+use crate::molt_lang::resolve::Resolver;
 use crate::rust_grammar::Ident;
 use crate::rust_grammar::Kind;
 use crate::rust_grammar::Node;
-use crate::rust_grammar::parse_node_with_kind;
 use crate::{Error, Input, Mode};
 
 const MAIN_FN_NAME: &str = "main";
@@ -103,121 +99,6 @@ pub struct Pat {
 impl std::fmt::Debug for Pat {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Pat").field("vars", &self.vars).finish()
-    }
-}
-
-type Result<T, E = parser::Error> = std::result::Result<T, E>;
-
-struct Resolver;
-
-impl Resolver {
-    fn resolve_file(&self, file: grammar::MoltFile) -> Result<MoltFile> {
-        let grammar::MoltFile { fns, stmts: _ } = file;
-        let fns: Vec<_> = fns
-            .into_iter()
-            .map(|f| self.resolve_fn(f))
-            .collect::<Result<_>>()?;
-        Ok(MoltFile { fns })
-    }
-
-    fn resolve_fn(&self, f: grammar::MoltFn) -> Result<MoltFn> {
-        Ok(MoltFn {
-            name: f.name,
-            args: f
-                .args
-                .into_iter()
-                .map(|a| self.resolve_fn_arg(a))
-                .collect::<Result<_>>()?,
-            stmts: f
-                .stmts
-                .into_iter()
-                .map(|s| self.resolve_stmt(s))
-                .collect::<Result<_>>()?,
-        })
-    }
-
-    fn resolve_fn_arg(&self, arg: grammar::FnArg) -> Result<FnArg> {
-        Ok(FnArg {
-            var_name: arg.var_name,
-            type_: self.resolve_type(arg.type_)?,
-        })
-    }
-
-    fn resolve_type(&self, arg: grammar::Type) -> Result<Type> {
-        Ok(match arg {
-            grammar::Type::Kind(ident) => Type::Kind(ident),
-        })
-    }
-
-    fn resolve_stmt(&self, stmt: grammar::Stmt) -> Result<Stmt> {
-        match stmt {
-            grammar::Stmt::ExprStmt(e) => Ok(Stmt::ExprStmt(self.resolve_expr(e)?)),
-            grammar::Stmt::Let(l) => Ok(Stmt::Let(self.resolve_let_stmt(l)?)),
-        }
-    }
-
-    fn resolve_let_stmt(&self, l: grammar::LetStmt) -> Result<LetStmt> {
-        let type_ = self.resolve_type(l.type_)?;
-        Ok(LetStmt {
-            lhs: self.resolve_let_lhs(l.lhs, &type_)?,
-            _type_: type_,
-            rhs: l.rhs.map(|e| self.resolve_expr(e)).transpose()?,
-        })
-    }
-
-    fn resolve_let_lhs(&self, lhs: grammar::LetLhs, type_: &Type) -> Result<LetLhs> {
-        Ok(match lhs {
-            grammar::LetLhs::Var(ident) => LetLhs::Var(ident),
-            grammar::LetLhs::Pat(pat) => LetLhs::Pat(self.resolve_pat(pat, type_)?),
-        })
-    }
-
-    fn resolve_fn_call(&self, f: grammar::FnCall) -> Result<FnCall> {
-        Ok(FnCall {
-            fn_name: f.fn_name,
-            args: f
-                .args
-                .into_iter()
-                .map(|e| self.resolve_expr(e))
-                .collect::<Result<_>>()?,
-        })
-    }
-
-    fn resolve_expr(&self, expr: grammar::Expr) -> Result<Expr> {
-        match expr {
-            grammar::Expr::FnCall(f) => Ok(Expr::FnCall(self.resolve_fn_call(f)?)),
-            grammar::Expr::Atom(name) => Ok(Expr::Atom(name)),
-        }
-    }
-
-    fn resolve_pat(&self, p: grammar::Pat, type_: &Type) -> Result<Pat> {
-        let mut pat_ctx = Ctx::<Node>::new(Mode::Molt);
-        for var in p.vars.into_iter() {
-            let kind = Kind::infer_from_name(&var.name.to_string())
-                .unwrap_or_else(|| panic!("For now, names need to be called like their kind. Identifier {:?} does not follow this rule.", var.name.to_string()));
-            pat_ctx.add_var::<Node>(Var::new(var.name, kind));
-        }
-        let ctx = Rc::new(RefCell::new(pat_ctx));
-        let Type::Kind(kind) = type_;
-        let node = crate::parser::parse_with_ctx(
-            ctx.clone(),
-            |stream| parse_node_with_kind(stream, *kind),
-            p.tokens,
-            Mode::Molt,
-        )?;
-        let ctx = ctx.replace(Ctx::new(Mode::Molt));
-        Ok(Pat {
-            vars: ctx
-                .iter_vars_ids()
-                .map(|(id, var)| TokenVar {
-                    id,
-                    _kind: var.kind(),
-                    ident: var.ident().clone(),
-                })
-                .collect(),
-            ctx,
-            node,
-        })
     }
 }
 
