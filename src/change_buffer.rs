08e5c7f7d257f4f7f5153bc6dba82abe64bc36fd
Toni Peter <mail@tonipeter.de>
2026-03-25 08:51:32 +0100

    While rewriting, add spaces in certain situations.
    The logic here is to hopefully only insert spaces if we have to: If the
    surrounding characters are alphanumeric and the new code is non empty

--- src/change_buffer.rs
+++ src/change_buffer.rs
@@ -1,3 +1,6 @@
+use std::borrow::Cow;
+use std::ops::Range;
+
 use crate::Span;
 
 #[derive(Debug, Clone)]
@@ -38,11 +41,11 @@ impl ChangeBuffer {
     }
 
     pub fn make_change(&mut self, span: Span, new_code: &str) {
-        let range = span.byte_range();
-        let len_delta: i32 = new_code.len() as i32 - (range.end - range.start) as i32;
         let span = self.translate_span(span);
         let range = span.byte_range();
-        self.code.replace_range(range.clone(), new_code);
+        let new_code = pad_replacement(&self.code, &range, new_code);
+        let len_delta = new_code.len() as i32 - (range.end - range.start) as i32;
+        self.code.replace_range(range.clone(), &new_code);
         self.changes.push(Change { span, len_delta });
     }
 
@@ -64,5 +67,27 @@ impl ChangeBuffer {
             }
         }
         pos
+    }
+}
+
+fn is_ident_char(b: u8) -> bool {
+    b.is_ascii_alphanumeric() || b == b'_'
+}
+
+fn pad_replacement<'a>(code: &str, range: &Range<usize>, new_code: &'a str) -> Cow<'a, str> {
+    if new_code.is_empty() {
+        return Cow::Borrowed(new_code);
+    }
+    let needs_leading_space = range.start > 0
+        && is_ident_char(code.as_bytes()[range.start - 1])
+        && is_ident_char(new_code.as_bytes()[0]);
+    let needs_trailing_space = range.end < code.len()
+        && is_ident_char(code.as_bytes()[range.end])
+        && is_ident_char(*new_code.as_bytes().last().unwrap());
+    match (needs_leading_space, needs_trailing_space) {
+        (false, false) => Cow::Borrowed(new_code),
+        (true, false) => Cow::Owned(format!(" {new_code}")),
+        (false, true) => Cow::Owned(format!("{new_code} ")),
+        (true, true) => Cow::Owned(format!(" {new_code} ")),
     }
 }
